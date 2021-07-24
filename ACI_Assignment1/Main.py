6129c0629f92f66a2f7ae37f68fe29ba6fef7aab
BANSAL <shiva.bansal@hp.com>
2021-07-24 19:41:13 +0530

    fixed some bugs

--- ACI_Assignment1/Main.py
+++ ACI_Assignment1/Main.py
@@ -17,7 +17,7 @@ class Queue:
     def __init__(self, list_type="stack", length=100):
         self.explore_list = []
         self.list_type = list_type
-        self.top = -1
+        self.top = 0
         self.bottom = 0
         self.length = length
 
@@ -56,7 +56,7 @@ class Queue:
         return node
 
 
-def board_test(size: [], goal_state, obstacles):
+def board(size: [], goal_state, obstacles):
 
     board_env = [[BlockState(0,0,(0,0)) for j in range(size[1])] for i in range(size[0])]
     row_val = 0
@@ -102,75 +102,14 @@ def board_test(size: [], goal_state, obstacles):
 
     return board_env
 
-def board(size: [], goal_state, obstacles):
 
-    board_env = [[BlockState(0,0,(0,0)) for j in range(size[1])] for i in range(size[0])]
-    row_val = 0
-    col_val = 0
-    for row in board_env:
-        for col in row:
-            h_value = abs((goal_state[0]- row_val)) + abs((goal_state[1]- col_val))
-            ## actions can be calulated and seems like not needed kept for now
-            actions = 4
-            if (row_val,col_val) in ((0,0),(0,7),(7,0),(7,7)): #corner cells
-                actions = 2
-            elif row_val in(0,7) or col_val in (0,7): #edge cells
-                actions = 3
-            else:
-                actions = 4
-            board_env[row_val][col_val] = BlockState(h_value, actions, (row_val,  col_val))
-            col_val += 1
-        col_val = 0
-        row_val += 1
-    for obstactle in obstactles:
-        for i in obstactle[1]:
-            board_env[i[0]][i[1]].cost = obstactle[3]
-            board_env[i[0]][i[1]].allowed = obstactle[2]
-            board_env[i[0]][i[1]].obstacle = obstactle[0]
-
-    row_val = 0
-    col_val = 0
-
-    for row in board_env:
-        for col in row:
-            if not board_env[row_val][col_val].allowed:
-                if col_val -1 >= 0 and board_env[row_val][col_val - 1].allowed is True:
-                    cost = board_env[row_val][col_val - 1].cost
-                    board_env[row_val][col_val - 1].cost = cost + board_env[row_val][col_val].cost
-                if col_val + 1 < 8 and board_env[row_val][col_val + 1].allowed is True:
-                    cost = board_env[row_val][col_val + 1].cost
-                    board_env[row_val][col_val + 1].cost = cost + board_env[row_val][col_val].cost
-                if row_val -1 >= 0 and board_env[row_val - 1][col_val].allowed is True:
-                    cost = board_env[row_val - 1][col_val - 1].cost
-                    board_env[row_val - 1][col_val].cost = cost + board_env[row_val][col_val].cost
-                if row_val + 1 < 8 and board_env[row_val + 1][col_val].allowed is True:
-                    cost = board_env[row_val + 1][col_val - 1].cost
-                    board_env[row_val + 1][col_val].cost = cost + board_env[row_val][col_val].cost
-                #diagonal cells
-                if row_val + 1 < 8 and col_val + 1 < 8 and board_env[row_val + 1][col_val + 1].allowed is True:
-                    cost = board_env[row_val + 1][col_val + 1].cost
-                    board_env[row_val + 1][col_val + 1].cost = cost + board_env[row_val][col_val].cost
-                if row_val + 1 < 8 and col_val - 1 >= 0 and board_env[row_val + 1][col_val - 1].allowed is True:
-                    cost = board_env[row_val + 1][col_val - 1].cost
-                    board_env[row_val + 1][col_val - 1].cost = cost + board_env[row_val][col_val].cost
-                if row_val - 1 >= 0 and col_val - 1 >= 0 and board_env[row_val - 1][col_val - 1].allowed is True:
-                    cost = board_env[row_val - 1][col_val - 1].cost
-                    board_env[row_val - 1][col_val - 1].cost = cost + board_env[row_val][col_val].cost
-                if row_val - 1 >= 0 and col_val + 1 < 8 and board_env[row_val - 1][col_val + 1].allowed is True:
-                    cost = board_env[row_val - 1][col_val - 1].cost
-                    board_env[row_val - 1][col_val + 1].cost = cost + board_env[row_val][col_val].cost
-            col_val += 1
-        col_val = 0
-        row_val += 1
-
-    return board_env
 
 def find_children(game_board, position):
 
     childerns = []
     if position[0] + 1 < 8 and game_board[position[0]+1][position[1]].allowed is True:
         childerns.append((position[0] + 1, position[1]))
-    if position[0] - 1 >= 8 and game_board[position[0]+1][position[1]].allowed is True:
+    if position[0] - 1 >= 0 and game_board[position[0]-1][position[1]].allowed is True:
         childerns.append((position[0] - 1, position[1]))
     if position[1] - 1 >= 0 and game_board[position[0]][position[1] - 1].allowed is True:
         childerns.append((position[0], position[1] - 1))
@@ -184,11 +123,8 @@ def find_children(game_board, position):
 def a_star(game_board, inital_position, goal_state):
 
     path = ""
-    returned_path = ""
     explore_list = Queue(list_type="priority_queue")
     explored_list = []
-
-
 
     if inital_position == goal_state:
         path = path + str(inital_position) + "|" + str(game_board[inital_position[0]][inital_position[1]].cost)
@@ -196,14 +132,14 @@ def a_star(game_board, inital_position, goal_state):
 
     explored_list.append((inital_position, path))
     children = find_children(game_board, inital_position)
+
     for child in children:
         if child != inital_position:
             game_board[child[0]][child[1]].g_value = game_board[inital_position[0]][inital_position[1]].cost + game_board[child[0]][child[1]].cost
-            game_board[child[0]][child[1]].root_path = [(inital_position, 0)]
+            game_board[child[0]][child[1]].root_path = str(inital_position) + "|" + str(0)
             explore_list.push((game_board[child[0]][child[1]], (inital_position, 0)))
     path = path + str(inital_position) + "|" + str(0)
     node = (game_board[inital_position[0]][inital_position[1]],path)
-
 
     while explore_list.top != explore_list.bottom:
 
@@ -218,13 +154,12 @@ def a_star(game_board, inital_position, goal_state):
         if path_reset:
             path = node[0].root_path
 
-        try:
-            tmp_list = [val[0] for val in explored_list]
-            tmp_list.index(node[0].position)
-            print("already explored")
-            print(node[0].position)
+        explored_flag = False
+        for val in explored_list:
+            if val[0] == node[0].position:
+                explored_flag = True
 
-        except:
+        if not explored_flag:
             if node[0].position == goal_state:
                 path = path + " -> " + str(node[0].position) + "|" + str(node[0].cost)
                 returned_path = path
@@ -236,16 +171,11 @@ def a_star(game_board, inital_position, goal_state):
 
             for child in children:
                 game_board[child[0]][child[1]].g_value = game_board[child[0]][child[1]].cost + node[0].g_value
-
                 if child != inital_position and game_board[child[0]][child[1]].pushed == False:
                     game_board[child[0]][child[1]].root_path = path
-                    root_paths[child] = path
                     explore_list.push((game_board[child[0]][child[1]], path))
                     game_board[child[0]][child[1]].pushed = True
-
-
-
-    return returned_path
+    return path
 
 
 knight_position = (0,0)
@@ -258,7 +188,7 @@ obstactles = [("wild animals", [(1,1), (1,2), (4,6), (4,7)], False, 5),
               ("mountains", [(1,5), (1,6),(3,3),(3,4)], False, 3),
               ]
 
-board_env = board_test([8,8], queen_position, obstactles)
+board_env = board([8,8], queen_position, obstactles)
 
 board_env[knight_position[0]][knight_position[1]].obstacle='K'
 board_env[queen_position[0]][queen_position[1]].obstacle='Q'
@@ -277,6 +207,7 @@ for i in board_env:
     print("")
 
 path = a_star(board_env, knight_position, queen_position)
+
 print(path)
 
 total_cost = 0
